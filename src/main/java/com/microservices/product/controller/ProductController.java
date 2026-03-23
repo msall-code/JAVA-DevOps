7de505262b312b99f09bd3aa015475b0939dcf0c
msall-code <mamadousalldk@gmail.com>
2026-03-23 13:16:41 +0000

    fix: crossorigin allow all origins

--- src/main/java/com/microservices/product/controller/ProductController.java
+++ src/main/java/com/microservices/product/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
--- src/main/java/com/microservices/product/controller/UserController.java
+++ src/main/java/com/microservices/product/controller/UserController.java
@@ -9,10 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = {
-        "http://localhost:3000",
-        "http://localhost:5173"
-})
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class UserController {
 
