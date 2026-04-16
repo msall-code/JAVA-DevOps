6ad42968ffb4193eb237186e4a9de22509491019
msall-code <mamadousalldk@gmail.com>
2026-04-16 16:51:02 +0000

    fix: final sync of monorepo structure

--- product-service/target/generated-sources/annotations/com/microservices/product/mapper/ProductMapperImpl.java
+++ product-service/target/generated-sources/annotations/com/microservices/product/mapper/ProductMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-04-15T18:28:50+0000",
+    date = "2026-04-16T16:50:49+0000",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.0.v20260407-0427, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
